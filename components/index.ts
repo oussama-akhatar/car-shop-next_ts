73f632dea8391f4b0af77b1b8fefe16313acdf3f
akhatar <o.akhatar5927@gmail.com>
2023-07-25 11:21:26 +0100

    Searchbar

--- components/index.ts
+++ components/index.ts
@@ -2,8 +2,11 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
-    CustomButton,Navbar,Footer
+    CustomButton,Navbar,Footer,SearchBar, CustomFilter, SearchManufacturer
 }
