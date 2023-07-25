944c1346255374bd43a70e3fba3992317e9de50b
akhatar <o.akhatar5927@gmail.com>
2023-07-25 09:49:13 +0100

    first commit

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    btnType: "button" | "submit";
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>
+}
